d8551d96fccb0f117ea0c4c1405b86dffa666a5b
WinterKuma <equsejhy@naver.com>
2023-04-26 03:02:51 +0900

    Utility 제작 ( AsyncEventExtensions, ConcurrentPool, PacketSegment, SwapChain )

---
+++ NetCoreMMOServer/NetCoreMMOServer.Utility/ConcurrentPool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreMMOServer.Utility
+{
+    public class ConcurrentPool<T> where T : new()
+    {
+        private ConcurrentBag<T> _pool = new();
+
+        public T Get()
+        {
+            if(_pool.TryTake(out var item)) 
+            { 
+                return item; 
+            }
+            else
+            {
+                return new T();
+            }
+        }
+
+        public void Return(T item)
+        {
+            _pool.Add(item);
+        }
+    }
+}
